d3e87ee922dfdc2f976b9c97aac0fde2e72cf9ed
vishnu <vishnu@vishnus-MacBook-Air.local>
2018-04-05 11:19:55 +0530

    Load data update

--- src/AdminBundle/Command/UnattendedCommand.php
+++ src/AdminBundle/Command/UnattendedCommand.php
@@ -3,6 +3,8 @@
 namespace AdminBundle\Command;
 
 use CommonBundle\Helpers\EntityHelper;
+use DBBundle\Entity\AdminUser;
+use DBBundle\Entity\Userrole;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -13,50 +15,53 @@ class UnattendedCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this
-            // the name of the command (the part after "bin/console")
-            ->setName('app:unattendedmail')
-            // the short description shown while running "php bin/console list"
-            ->setDescription('Mail for unattended services')
+            // the name of the command (the part after "app/console")
+            ->setName('Login:loaddata')
+            // the short description shown while running "php app/console list"
+            ->setDescription('Create local users')
             // the full command description shown when running the command with
             // the "--help" option
-            ->setHelp('This command allows you to send mail for unattended service');
+            ->setHelp('This command allows you to create dummy data for login');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $em = $this->getContainer()->get('doctrine')->getManager();
-        $finddata = $em->getRepository('DBBundle:Jobs')->pendingjob();
-        $to = array();
-        $machines = array();
-        $subject = 'Unattended Service Requests';
-        $transport = (new \Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
-            ->setUsername('vs@gmail.com')
-            ->setPassword('password');
-        $mailer = new \Swift_Mailer($transport);
-        foreach ($finddata as $usersdata) {
-            $findjobsdataobj = $em->getRepository('DBBundle:Jobs')->findOneBy(array('id' => $usersdata['id']));
-            if ($findjobsdataobj) {
-                $findjobsdataobj->setJobemail(1);
-                $em->merge($findjobsdataobj);
-                $em->flush();
-            }
-            $to[] = 'vs@gmail.com';
-            $todata = array_unique($to);
-            if (count($todata) > 0) {
-                $message = (new \Swift_Message($subject))
-                    ->setFrom(['vs@gmail.com' => 'VS'])
-                    ->setTo($todata)
-                    ->setBody($this->getContainer()->get('templating')->render(
-                        'ServiceBundle:Default:jobpendingemail.html.twig',
-                        array('jobdata' => $finddata, 'jobmachine' => $machines)
-                    ), 'text/html');
-
-                $resultdata = $mailer->send($message);
-            }
+        $finddata = $em->getRepository('DBBundle:AdminUser')->findOneBy(array('email'=>'demo@gmail.com'));
+        if($finddata){
             // outputs a message followed by a "\n"
-            $output->writeln('Task Complete');
-
-
+            $output->writeln('Demo users already inside database!');
+        }else{
+            $roleobj = new Userrole();
+            $roleobj->setName('Admin');
+            $roleobj->setDescription('Role For Admin');
+            $roleobj->setStatus(1);
+            $roleobj->setIsdeleted(0);
+            $roleobj->setDatecreated(new \DateTime());
+            $roleobj->setDatemodified(new \DateTime());
+            $roleobj->setCreatedby(1);
+            $roleobj->setModifiedby(1);
+            $em->persist($roleobj);
+            $obj = new AdminUser();
+            $obj->setName('Demo Hero');
+            $obj->setEmail('demo@gmail.com');
+            $obj->setStatus(1);
+            $obj->setMobile('9412558532');
+            $obj->setPassword(md5(123456));
+            $obj->setDatecreated(new \DateTime());
+            $obj->setDatemodified(new \DateTime());
+            $obj->setCreatedby(1);
+            $obj->setModifiedby(1);
+            $obj->setUserrole($roleobj);
+            $em->persist($obj);
+            $em->flush();
+            // outputs a message followed by a "\n"
+            $output->writeln('Command successfully execute.Login Credential: Username: demo@gmail.com and password: 123456');
         }
+
+
+
+
+
     }
 }
